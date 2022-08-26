12d0fb92f687d39ca535e0eafa5160d5adf5f292
Bruna Lopes <b.lopesassis@hotmail.com>
2022-08-26 15:24:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function voltarParaTopo() {
+    document.documentElement.scrollTop = 0;
+}
+
+
+//identificar o clique no menu
+const menuItems = document.querySelectorAll('.icons a');
+//console.log(menuItems);
+
+//verificar se cada item foi clicado
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick)
+})
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    //console.log(event)
+    const element = event.target;
+    //console.log(element)
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+    
+
+    window.scroll({
+        top: to -50,
+        behavior: "smooth",
+    })
+}
+
