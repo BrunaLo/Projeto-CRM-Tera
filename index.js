1c4135468e2d4eb41b6f92c896b70cfebcf800e8
Bruna Lopes <b.lopesassis@hotmail.com>
2022-08-30 14:42:12 -0300

    alteração de documentos

--- index.js
+++ index.js
@@ -3,27 +3,5 @@ function voltarParaTopo() {
 }
 
 
-//identificar o clique no menu
-const menuItems = document.querySelectorAll('.icons a');
-//console.log(menuItems);
 
-//verificar se cada item foi clicado
-menuItems.forEach(item => {
-    item.addEventListener('click', scrollToIdOnClick)
-})
-
-function scrollToIdOnClick(event) {
-    //event.preventDefault();
-    //console.log(event)
-    const element = event.target;
-    //console.log(element)
-    const id = element.getAttribute('href');
-    const to = document.querySelector(id).offsetTop;
-    
-
-    window.scroll({
-        top: to -70,
-        behavior: "smooth",
-    })
-}
 
